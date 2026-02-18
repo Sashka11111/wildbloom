14b2d5542b26c22faabe441910f40a43d4d1da40
Sashka11111 <lamcevasaska@gmail.com>
2026-02-18 09:44:29 +0200

    Implement react-router navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import AboutPage from './pages/AboutPage/AboutPage'
 import FaqPage from './pages/FaqPage/FaqPage'
 import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home')
-
-  const navigate = (pageName) => {
-    setCurrentPage(pageName)
-    window.scrollTo(0, 0)
-  }
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'about':
-        return <AboutPage />
-      case 'faq':
-        return <FaqPage />
-      case 'auth':
-        return <LoginPage />
-      default:
-        return <HomePage />
-    }
-  }
-
   return (
-    <div className="app">
-      <nav>
-        <button onClick={() => navigate('home')}>Home</button>
-        <button onClick={() => navigate('about')}>About</button>
-        <button onClick={() => navigate('faq')}>FAQ</button>
-        <button onClick={() => navigate('auth')}>Login</button>
-      </nav>
-
-      <main>{renderPage()}</main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/faq" element={<FaqPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
